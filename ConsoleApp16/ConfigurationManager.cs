04bcab6ef994ac93e577baaf382600fd1c36170b
Denys Dronov <dronovdenis36@gmail.com>
2023-11-24 20:24:15 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp16/ConfigurationManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class ConfigurationManager
+    {
+        private static ConfigurationManager _instance;
+        private string loggingMode;
+        private string databaseConnectionString;
+
+        private ConfigurationManager() { }
+
+        public static ConfigurationManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new ConfigurationManager();
+                }
+                return _instance;
+            }
+        }
+
+        public string LoggingMode
+        {
+            get { return loggingMode; }
+            set
+            {
+                loggingMode = value;
+                Console.WriteLine("Logging mode set to: " + loggingMode);
+            }
+        }
+
+        public string DatabaseConnectionString
+        {
+            get { return databaseConnectionString; }
+            set
+            {
+                databaseConnectionString = value;
+                Console.WriteLine("Database connection string set to: " + databaseConnectionString);
+            }
+        }
+    }
+
+}
+
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp16;
+
+class Program
+{
+    static void Main()
+    {
+        ConfigurationManager configManager = ConfigurationManager.Instance;
+
+        Console.WriteLine("Enter logging mode:");
+        configManager.LoggingMode = Console.ReadLine();
+
+        Console.WriteLine("Enter database connection string:");
+        configManager.DatabaseConnectionString = Console.ReadLine();
+
+        Console.ReadLine(); 
+    }
+}
